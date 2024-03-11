c2f5637cb86b8a115d2c2a3cb1f178b1d7fd5e87
HirschLPYT <duernbergermanuel2@gmail.com>
2024-03-11 12:14:54 +0100

    Added ASPdotNet Server

---
+++ ASP_Server/Program.cs
@@ -0,0 +1,15 @@
+namespace ASP_Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hallo Welt!");
+
+            app.Run();
+        }
+    }
+}
