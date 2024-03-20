94eef6609649a38889eb79b4d8273c2c69ce5f1d
HirschLPYT <duernbergermanuel2@gmail.com>
2024-03-20 16:46:56 +0100

    Added MongoDB ASPdotNet Application

---
+++ NotableApi/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NotableApi.Models
+{
+    public class Note
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Title")]
+        public string Title { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Content { get; set; } = null!;
+    }
+}
