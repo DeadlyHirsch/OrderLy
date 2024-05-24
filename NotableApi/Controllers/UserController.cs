0db654d365f569bc2a9fbaead935b266c9c04060
HirschLPYT <duernbergermanuel2@gmail.com>
2024-05-24 09:55:14 +0200

    Add HTTP Request to Login Button

--- NotableApi/Controllers/UserController.cs
+++ NotableApi/Controllers/UserController.cs
@@ -3,7 +3,6 @@ using NotableApi.Models;
 using NotableApi.Services;
 using System.Security.Cryptography;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NotableApi.Controllers
 {
--- Notable_WPF_Client/LoginWindow.xaml.cs
+++ Notable_WPF_Client/LoginWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +21,11 @@ namespace Notable_WPF_Client
     /// </summary>
     public partial class LoginWindow : Window
     {
+        static HttpClient client = new HttpClient();
         public LoginWindow()
         {
             InitializeComponent();
+            client.BaseAddress = new Uri("http://127.0.0.1:7180");
         }
 
         private void BTNRegister_Click(object sender, RoutedEventArgs e)
@@ -29,9 +33,34 @@ namespace Notable_WPF_Client
 
         }
 
-        private void BTNLogin_Click(object sender, RoutedEventArgs e)
+        private async void BTNLogin_Click(object sender, RoutedEventArgs e)
         {
+            User user = new(TBUsername.Text, TBPassword.Text);
 
+            var url = await CreateUserAsync(user);
+        }
+
+        static async Task<Uri> CreateUserAsync(User user)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/User", user);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        public class User
+        {
+            public string? Id { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+
+            public User(string name, string pw)
+            {
+                UserName = name;
+                Password = pw;
+            }
         }
     }
 }
