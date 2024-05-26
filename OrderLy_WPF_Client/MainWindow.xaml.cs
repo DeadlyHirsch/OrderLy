49a2cdfe93868771ae2415b6877cd02453bd5694
HirschLPYT <duernbergermanuel2@gmail.com>
2024-05-27 00:02:08 +0200

    Added WPF Application

---
+++ OrderLy_WPF_Client/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderLy_WPF_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string CurrentDate = DateTime.Now.ToString().Split(' ')[0];
+            string WeekDay = DateTime.Now.DayOfWeek.ToString();
+            LBLDate.Content = $"{CurrentDate}, {WeekDay}";
+            LoadChartData();
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            LBLDate.FontSize = LBLDate.ActualHeight * 0.7;
+            LBLRecent.FontSize = LBLRecent.ActualHeight * 0.7;
+            LBLOrderLy.FontSize = LBLOrderLy.ActualHeight * 0.7;
+        }
+
+        private string LoadChartData()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("OrderLy");
+            var collection = db.GetCollection<BsonDocument>("Orders");
+
+            var Result = collection.Aggregate()
+                .Match(new BsonDocument { { "createdAt",
+                new BsonDocument("$gte", DateTime.Now.AddDays(-365)) } })
+                .Group(new BsonDocument{
+                    { "_id",
+                        new BsonDocument("$dateToString",
+                        new BsonDocument
+                        {
+                            { "format", "%V" },
+                            { "date", "$createdAt" }
+                        })
+                    },
+                { "count",
+                    new BsonDocument("$sum", 1) }
+                })
+                .ToList()
+                .ToJson();
+
+            return Result;
+        }
+    }
+}
---
+++ OrderLy_WPF_Client/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.Painting.Effects;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderLy_WPF_Client
+{
+    public class ViewModel
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] {2,4,1,2,3,2,4,1,2,3,4,5,1,2,4,1,2,3,2,4,1,2,3,4,5,1,2,4,1,2,3,2,4,1,2,3,4,5,1,2,4,1,2,3,2,4,1,2,3,4,5,1},
+                    Fill = null,
+                    LineSmoothness = 0.8
+                }
+            };
+
+        public Axis[] XAxes { get; set; }
+        = new Axis[]
+        {
+            new Axis
+            {
+                Name = "Calendar Week",
+                NamePaint = new SolidColorPaint(SKColors.White),
+                LabelsPaint = new SolidColorPaint(SKColors.White),
+                TextSize = 10,
+                SeparatorsPaint = new SolidColorPaint(SKColors.White)
+            }
+
+        };
+
+        public Axis[] YAxes { get; set; }
+            = new Axis[]
+            {
+                new Axis
+                {
+                    Name = "# of Orders",
+                    NamePaint = new SolidColorPaint(SKColors.White),
+
+                    LabelsPaint = new SolidColorPaint(SKColors.White),
+                    TextSize = 10,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray)
+                    {
+                        StrokeThickness = 2,
+                        PathEffect = new DashEffect(new float[] { 3, 3 })
+                    }
+                }
+            };
+    }
+}
